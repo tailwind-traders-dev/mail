6f19448f865112bf2c39094704b49523be8f6597
Rob Conery <rob@conery.io>
2023-10-31 11:04:41 -0700

    fixed dotenv

--- server/DotEnv.cs
+++ server/DotEnv.cs
@@ -10,18 +10,22 @@ public static class DotEnv
         var execDirectory = Directory.GetCurrentDirectory();
         string projectDirectory = Directory.GetParent(execDirectory).Parent.Parent.FullName;
         var filePath = Path.Combine(projectDirectory, ".env");
-        if (!File.Exists(filePath))
-            return;
+        if (!File.Exists(filePath)) return;
         //Console.WriteLine("Loading .env file");
         foreach (var line in File.ReadAllLines(filePath))
         {
 
-          var parts = line.Split('=',StringSplitOptions.RemoveEmptyEntries);
-          
-          if (parts.Length != 2)
-              continue;
-          //Console.WriteLine("Setting {0} to {1}", parts[0], parts[1]);
-          Environment.SetEnvironmentVariable(parts[0], parts[1].Replace("\"", ""));
+          if(line.IndexOf("#") == 0) continue; //it's a comment
+
+          //get the first index of = and split on that; the values might have = in them so split won't work
+          var idx = line.IndexOf('=');
+          //var parts = line.Split('=',StringSplitOptions.RemoveEmptyEntries);
+          var key = line.Substring(0, idx);
+          var val = line.Substring(idx + 1);
+
+          //if (parts.Length != 2)continue;
+          //Console.WriteLine("Setting {0} to {1}", key, val);
+          Environment.SetEnvironmentVariable(key, val.Replace("\"", ""));
         }
     }
 }
