f5193106ac4bcec8c5bc1d1ff9011fcc535408c7
Rob Conery <rob@conery.io>
2024-01-08 11:27:04 -0800

    Added extremely simple data stuff for commands and so on

---
+++ server/Tests/Basic_Commands.cs
@@ -0,0 +1,25 @@
+using Xunit;
+using Tailwind.Mail.Services;
+using Tailwind.Mail.Commands;
+namespace Tailwind.Mail.Tests;
+
+public class Basic_Commands:TestBase
+{
+  [Fact]
+  public async Task A_contact_is_deleted_created_and_updated()
+  {
+    var cmd = new Command();
+    var vals =     new Dictionary<string,object>{
+      {"name", "Test User"},
+      {"email", "test@test.com "}
+    };
+    cmd.Delete("mail.contacts", vals);
+    cmd.Insert("mail.contacts", vals);
+    cmd.Update("mail.contacts", 1, new Dictionary<string,object>{
+      {"name", "Test User 2"},
+      {"email", "test2@test.com "}
+    });
+    var results = await cmd.Execute();
+    Assert.Equal(3, results);
+  }
+}
--- server/Tests/Jon_Sends_a_Broadcast.cs
+++ server/Tests/Jon_Sends_a_Broadcast.cs
@@ -1,52 +1,52 @@
-using Xunit;
-using Tailwind.Mail.Data;
-using Tailwind.Mail.Data.Models;
-using Microsoft.EntityFrameworkCore;
-using Tailwind.Mail.Services;
-namespace Tailwind.Mail.Tests;
+// using Xunit;
+// using Tailwind.Mail.Data;
+// using Tailwind.Mail.Data.Models;
+// using Microsoft.EntityFrameworkCore;
+// using Tailwind.Mail.Services;
+// namespace Tailwind.Mail.Tests;
 
-public class Jon_Sends_a_Broadcast:TestBase
-{
+// public class Jon_Sends_a_Broadcast:TestBase
+// {
 
-  [Fact]
-  public async Task ten_thousand_messages_are_queued()
-  {
-
-    //add 10,000 contacts with "Test" tag
-    var tag = new Tag{Name="Test", Slug="test"};
-
-    //create a list of contacts with the tag
-    var _contacts = new List<Contact>();
-    for(var i = 0; i < 10000; i++){
-      var c = new Contact{
-        Email = $"test{i}@test.com",
-        Subscribed = true
-      };
-      c.Tags.Add(tag);
-      _contacts.Add(c);
-    }
-
-    var broadcast = Broadcast.Create("test","This is a test","# Hello");
-    var messages = Message.CreateForBroadcast(broadcast, _contacts);
-
-    broadcast = Outbox.Queue(broadcast, messages);
-  }
-}
-// public class ContactBasicTests {
-//   Db _db;
-//   Contact _contact;
-//   public ContactBasicTests()
-//   {
-
-//     _db = new Db();
-//     _contact = new Contact{Email="test@test.com", Name="Test"};
-//     _db.Add(_contact);
-//     _db.SaveChanges();
-//   }  
 //   [Fact]
-//   public void The_contact_is_saved_with_an_id()
+//   public async Task ten_thousand_messages_are_queued()
 //   {
-//     //there should be one doc in there
-//     Assert.Equal(_contact.ID, 1);
+
+//     //add 10,000 contacts with "Test" tag
+//     var tag = new Tag{Name="Test", Slug="test"};
+
+//     //create a list of contacts with the tag
+//     var _contacts = new List<Contact>();
+//     for(var i = 0; i < 10000; i++){
+//       var c = new Contact{
+//         Email = $"test{i}@test.com",
+//         Subscribed = true
+//       };
+//       c.Tags.Add(tag);
+//       _contacts.Add(c);
+//     }
+
+//     var broadcast = Broadcast.Create("test","This is a test","# Hello");
+//     var messages = Message.CreateForBroadcast(broadcast, _contacts);
+
+//     broadcast = Outbox.Queue(broadcast, messages);
 //   }
 // }
+// // public class ContactBasicTests {
+// //   Db _db;
+// //   Contact _contact;
+// //   public ContactBasicTests()
+// //   {
+
+// //     _db = new Db();
+// //     _contact = new Contact{Email="test@test.com", Name="Test"};
+// //     _db.Add(_contact);
+// //     _db.SaveChanges();
+// //   }  
+// //   [Fact]
+// //   public void The_contact_is_saved_with_an_id()
+// //   {
+// //     //there should be one doc in there
+// //     Assert.Equal(_contact.ID, 1);
+// //   }
+// // }
